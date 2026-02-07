238b045979f87c6b06f4b231e56d71a664c7202d
Himanshu Upadhyay <himanshuupadhyay@Himanshus-MacBook-Air.local>
2026-02-07 20:47:44 +0530

    Add initial backend files

---
+++ fruits/index.js
@@ -0,0 +1,5 @@
+const apple=require("./apple");
+const banana=require("./banana");
+const orange=require("./orange");
+ fruits=[apple,banana,orange];
+ module.exports= fruits;
---
+++ script.js
@@ -0,0 +1,9 @@
+// let n=5;
+// for(let i=1;i<=5;i++){
+//     console.log("hello",i);
+// }
+//  const somevalue=require("./math");
+//  console.log(somevalue);
+
+const fruits = require("./fruits");
+console.log(fruits);
